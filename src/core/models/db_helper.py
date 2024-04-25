de500dbe54084e60fab4b13f6f47963cb941a25b
User <mrlivedance@vk.com>
2024-04-25 19:23:50 +0300

    add docstrings

--- src/core/models/db_helper.py
+++ src/core/models/db_helper.py
@@ -5,7 +5,16 @@ from src.core.config import settings
 
 class DatabaseHelper:
     def __init__(self, url: str, echo: bool = False):
+        """
+        Класс помощник для работы с подключением к базе данных
+
+        :param url: url базы данных указанный в конфиге проекта
+        :param echo: вывод в консоль запросов к базе данных
+        """
+        # Создание движка
         self.engine = create_async_engine(url=url, echo=echo)
+
+        # Создание асинхронных сессии
         self.session_factory = async_sessionmaker(
             bind=self.engine,
             expire_on_commit=False,
