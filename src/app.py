05c761401cb86182efb29f11ceee74357fb6a58b
user <mrlivedance@vk.com>
2024-08-13 12:59:27 +0300

    del lifespan

--- src/app.py
+++ src/app.py
@@ -10,17 +10,7 @@ from src.repositories.models import Task, User
 from src.routing.task import task_router
 
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    """Создание таблиц перед запуском приложения"""
-    async with db_helper.engine.begin() as conn:
-        # await conn.run_sync(Task.metadata.drop_all)
-        await conn.run_sync(User.metadata.create_all)
-        await conn.run_sync(Task.metadata.create_all)
-    yield
-
-
-app = FastAPI(lifespan=lifespan)
+app = FastAPI()
 fastapi_users = FastAPIUsers[UserSchemas, int](
     get_user_manager,
     [auth_backend],
