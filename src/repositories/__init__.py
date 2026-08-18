0472faacc4b6d539314042e4ccfbe960436211b4
ilakovalcuk <mrlivedance@vk.com>
2026-08-18 20:44:30 +0300

    Refactor model imports in repositories to improve structure and clarity

--- src/repositories/__init__.py
+++ src/repositories/__init__.py
@@ -1,3 +1,3 @@
-__all__ = ("db_helper",)
+from src.repositories.db_helper import db_helper
 
-from .db_helper import db_helper
+__all__ = ("db_helper",)
--- src/repositories/models/__init__.py
+++ src/repositories/models/__init__.py
@@ -1,9 +1,15 @@
-__all__ = (
-    "Task",
-    "User",
-    "Base",
-)
+from src.repositories.models.base import Base
+from src.repositories.models.enums import TaskPriority, TaskStatus, UserRole
+from src.repositories.models.refresh_token import RefreshToken
+from src.repositories.models.task import Task
+from src.repositories.models.user import User
 
-from .task import Task
-from .user import User
-from .base import Base
+__all__ = (
+    "Base",
+    "RefreshToken",
+    "Task",
+    "TaskPriority",
+    "TaskStatus",
+    "User",
+    "UserRole",
+)
